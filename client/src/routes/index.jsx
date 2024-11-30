2e9e3d1a57f1997bac73bd5ad945e4ff026490c6
albanovs <albvnovs@gmail.com>
2024-11-30 09:00:18 +0600

    hashrouter

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import MainRoutes from './MainRoutes';
 import LoginRoutes from './AuthenticationRoutes';
 
-const router = createBrowserRouter([MainRoutes, LoginRoutes], {
+const router = createHashRouter([MainRoutes, LoginRoutes], {
   basename: import.meta.env.VITE_APP_BASE_NAME || '/'
 });
 
