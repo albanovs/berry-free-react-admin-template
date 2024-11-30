bc37ecc8c5b9b6706b81500400c213784dcd8307
albanovs <albvnovs@gmail.com>
2024-11-30 09:41:32 +0600

    erp

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -2,11 +2,11 @@ import { createHashRouter } from 'react-router-dom';
 import MainRoutes from './MainRoutes';
 import LoginRoutes from './AuthenticationRoutes';
 
-const router = createHashRouter({
-  ...MainRoutes,
-  ...LoginRoutes
-}, {
-  basename: import.meta.env.VITE_APP_BASE_NAME
+const router = createHashRouter([MainRoutes, LoginRoutes], {
+  basename: import.meta.env.VITE_APP_BASE_NAME,
+  future: {
+    v7_startTransition: true,
+  },
 });
 
 export default router;
